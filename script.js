be8b419ba9ce8cd0a4f6f2579f70977c3507b1a9
Ruslan Bets <betsruslanjs@gmail.com>
2021-11-01 19:46:42 +0300

    rotatePhotos

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const photos = document.querySelectorAll('.wrapper img')
 const glass = document.querySelector('.glass')
 const photo = glass.firstChild
+let rotationAngle = 0
+let screenRatio,photoRatio
+
+ 
 
 photos.forEach(photo => photo.onclick = handleClickToEnlarge)
 
 glass.onclick = hidePhoto
+
+document.body.onkeydown = e => {
+  if (e.key === 'ArrowRight') rotatePhoto()
+  else if (e.key === 'ArrowLeft') rotatePhoto(true)
+}
 
 function handleClickToEnlarge(e) {
   showPhoto(e.target.src)
@@ -18,4 +27,18 @@ function showPhoto(path) {
 
 function hidePhoto() {
   glass.hidden = true
+}
+
+function rotatePhoto(anticlockwise = false) {
+  rotationAngle = rotationAngle + 90 - 180 * anticlockwise
+  photo.style.transform = `rotate(${rotationAngle}deg)`
+}
+
+
+function updatePhotoRatio() {
+
+}
+
+function updateScreenRatio() {
+  screenRatio = innerWidth / innerHeight
 }
